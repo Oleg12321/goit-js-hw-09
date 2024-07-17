14a3e920d3e44798c33cc16684a81822e5b3cdd1
Oleg Danylovych <olegdanilovic19@gmail.com>
2024-07-17 19:54:38 +0300

    commit

---
+++ src/js/2-form.js
@@ -0,0 +1,39 @@
+const form = document.querySelector('form');
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const savedSettings = localStorage.getItem('feedback-form-state');
+const localStorageKey = 'feedback-form-state';
+
+if (savedSettings) {
+  const parsedSettings = JSON.parse(savedSettings);
+  form.elements.email.value = parsedSettings.email ?? '';
+  form.elements.message.value = parsedSettings.message ?? '';
+}
+
+form.addEventListener('input', e => {
+  e.preventDefault();
+  formData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  if (form.elements.email.value === '' || form.elements.message.value === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+  formData = {
+    email: '',
+    message: '',
+  };
+});
